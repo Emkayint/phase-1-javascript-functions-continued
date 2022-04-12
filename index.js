9c8db620f1a6e4255fa48c88786573761eeabf9f
Martin <mkmartiones@gmail.com>
2022-04-12 14:42:52 +0300

    Finished Lab

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ function saturdayFun(activity = "roller-skate"){
     return `This Saturday, I want to ${activity}!`;
 }
 
-console.log(saturdayFun())
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(param){
+    return function(param2){
+        return `You are ${param}${param2}${param}!`
+    }
+}
